050f3171341bf541ff9abb7bc3aa342ef55761b1
Lee <Lee@LeedeAir-2.attlocal.net>
2016-11-15 17:49:51 -0800

    updated to alternative method

--- MFA/R/mfa.r
+++ MFA/R/mfa.r
@@ -1,18 +1,72 @@
+X: the whole data
+X<-data.matrix(data)
 
-# create a s4 class "mfa"
-setClass(
-Class="mfa",
-slots=list(eigenvalues="numeric",common_factor_score="matrix",partial_factor_score="matrix",loadings="matrix")
-)
-# constructor function: to construct 'mfa' and run the model
-mfa<-function(data,sets,ncomps=NULL,center=TRUE,scale=TRUE){
+# Construct S=XAXt
+S<-as(X %*% A %*% t(X),"matrix")
+
+# do spectral decomposition on S: S=P*LAMBDA*Pt, PtMP=I
+# construct inverse delta: delta^2=LAMBDA
+eigens<-eigen(S)
+d<-matrix(0,dim(X)[1],dim(X)[1])
+for (i in 1:length(eigens$values)){
+    d[i,i]<-eigens$values[i]
+}
+u<-eigens$vectors
+print(eigens$values)
+lambda<-as(M_half %*% d %*% M_half,"matrix")
+
+delta_value<-diag(as(sqrt(lambda),"matrix"))
+
+delta_inv<-1/sqrt(lambda)
+delta_inv[is.infinite(delta_inv)]<-0
+
+# P is PMPt=I FOR S=P*LAMBDA*Pt
+P <- as(u %*% M_half_inv,"matrix")
+print("P")
+print(P[,1:2])
+# Q FOR Q=Xt*M*P*DELTA_inverse
+Q <- as(t(X) %*% M %*% P %*% delta_inv, "matrix")
+
+
+
+# build a list: 'partial_factor_score' to store partial factor score
+# build a matrix: 'common_factor_score' to store common factor score
+# the partial score of group i is named "Partial Score: Group i"
+# partial factor score i = no. of group * A_i* data group i* Q_i
+# common factor score = sum of partial factor score i
+
+
+partial_factor_score<-list()
+common_factor_score<-0
+for (i in 1:length(sets)){
+    datai<- data.matrix(eval(parse(text=paste0("Group",i))))
+    score<-length(sets) * (1/singularvalues[i]^2) * datai %*% t(datai)  %*% M %*% P %*% delta_inv
+    partial_factor_score[[paste0("Partial Score: Group ",i)]]=as(score,"matrix")
+    common_factor_score<-score+common_factor_score
+}
+common_factor_score<-common_factor_score/length(sets)
+
+
+# loading uses Q
 new (Class = "mfa",
-eigenvalues = data,
-common_factor_score = matrix(data, nrow = 2, ncol = 2),
-partial_factor_score = matrix(data, nrow = 2, ncol = 2),
-loadings = matrix(data, nrow = 2, ncol = 2)
+eigenvalues = delta_value^2,
+common_factor_score = as(common_factor_score,"matrix"),
+partial_factor_score = partial_factor_score,
+loadings = as(Q,"matrix")
 )
 }
+
+######################### test ###################
+#load wine data
+data<-read.csv("wine.csv",header=T,stringsAsFactors=F)
+datas<-data[,2:54]
+ndatas<-apply(datas,2,function(x){ (x-mean(x))/norm(x-mean(x),type="2")})
+test<-mfa(ndatas,sets=c(6,6,6,5,6,5,4,6,5,4),center=FALSE,scale=FALSE)
+
+
+################ supplementary method #############
+
+
 # set print() to print basic infomation
 setMethod("print",
 signature="mfa",
@@ -47,3 +101,5 @@ function(object,sets){
 
 # set funtion LG() to take two tables and return lg coefficient
 LG<-function(table1,table2){}
+
+# Bootstrap?
