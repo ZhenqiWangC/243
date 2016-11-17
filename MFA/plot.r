72d74548b5c076143c3d9b4dcf8e76391428d765
Lee <Lee@LeedeAir-2.attlocal.net>
2016-11-17 15:42:45 -0800

    combined methods in model2.Rmd

--- MFA/plot.r
+++ MFA/plot.r
@@ -1,47 +1,48 @@
 # plot method and functions for plot mfa
 partial_plot<-function(group_num,data,data2,names){
   ggplot2::ggplot()+
-    ggplot2::geom_point(data=data,aes(x=data[,1],y=data[,2],color=id),size=2)+
-    ggplot2::geom_point(data=data2,aes(x=data2[,1],y=data2[,2]),color="grey10",shape=17,size=1.5)+
-    ggplot2::geom_text(data=data2,aes(x=data2[,1],y=data2[,2],label=id),size=2,color="black",hjust=-0.15, vjust=-0.05)+
+    ggplot2::geom_point(data=data,ggplot2::aes(x=data[,1],y=data[,2],color=id),size=2)+
+    ggplot2::geom_point(data=data2,ggplot2::aes(x=data2[,1],y=data2[,2]),color="grey10",shape=17,size=1.5)+
+    ggplot2::geom_text(data=data2,ggplot2::aes(x=data2[,1],y=data2[,2],label=id),size=2,color="black",hjust=-0.15, vjust=-0.05)+
     ggplot2::scale_shape_manual(values=group_num+1)+
-    ggplot2::theme(plot.title = element_text(size=8, face="bold",vjust=0.05,color="grey40"),
-                   axis.title.x = element_text(size=8, face="bold",vjust=-0.05,color="grey40"),
-                   axis.title.y = element_text(size=8, face="bold",vjust=0.05,color="grey40"),
+    ggplot2::theme(plot.title = ggplot2::element_text(size=8, face="bold",vjust=0.05,color="grey40"),
+                   axis.title.x = ggplot2::element_text(size=8, face="bold",vjust=-0.05,color="grey40"),
+                   axis.title.y = ggplot2::element_text(size=8, face="bold",vjust=0.05,color="grey40"),
                    legend.position="none",
-                   axis.text.x = element_text(color="grey40",size=8),
-                   axis.text.y = element_text(color="grey40",size=8),
-                   panel.grid.minor = element_blank(),
-                   panel.grid.major = element_line(colour = "grey90",size=0.1),
-                   panel.background = element_blank())+
+                   axis.text.x = ggplot2::element_text(color="grey40",size=8),
+                   axis.text.y = ggplot2::element_text(color="grey40",size=8),
+                   panel.grid.minor = ggplot2::element_blank(),
+                   panel.grid.major = ggplot2::element_line(colour = "grey90",size=0.1),
+                   panel.background = ggplot2::element_blank())+
     # plot x and y axis
     ggthemes::scale_color_calc()+
-    ggplot2::annotate("segment", x=-Inf,xend=Inf,y=0,yend=0,arrow=arrow(length=unit(0.2,"cm")),size=0.3,color="grey50") +
-    ggplot2::annotate("segment", y=-Inf,yend=Inf,x=0,xend=0,arrow=arrow(length=unit(0.2,"cm")),size=0.3,color="grey50")+
+    ggplot2::annotate("segment", x=-Inf,xend=Inf,y=0,yend=0,arrow=ggplot2::arrow(length=ggplot2::unit(0.2,"cm")),size=0.3,color="grey50") +
+    ggplot2::annotate("segment", y=-Inf,yend=Inf,x=0,xend=0,arrow=ggplot2::arrow(length=ggplot2::unit(0.2,"cm")),size=0.3,color="grey50")+
     # center (0,0)
     ggplot2::xlim(-max(abs(c(data[,1],data2[,1])))*1.2, max(abs(c(data[,1],data2[,1])))*1.2)+
     ggplot2::ylim(-max(abs(c(data[,2],data2[,2])))*1.2, max(abs(c(data[,2],data2[,2])))*1.2)+
     ggplot2::ggtitle(paste0("Partial Factor Score: Group ",group_num))+ggplot2::labs(x=names[1],y=names[2])
 }
 
-blankPlot <- ggplot2::ggplot()+ggplot2::geom_blank(aes(1,1))+
-    ggplot2::theme(
-    plot.background = element_blank(),
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    panel.border = element_blank(),
-    panel.background = element_blank(),
-    axis.title.x = element_blank(),
-    axis.title.y = element_blank(),
-    axis.text.x = element_blank(),
-    axis.text.y = element_blank(),
-    axis.ticks = element_blank(),
-    axis.line = element_blank()
-  )
 
-setGeneric("plot",function(object,dim,eigen)standardGeneric("plot"))
+setGeneric("plot",function(object,dim)standardGeneric("plot"))
 setMethod("plot",signature="mfa",
        function(object,dim){
+         blankPlot <- ggplot2::ggplot()+ggplot2::geom_blank(ggplot2::aes(1,1))+
+           ggplot2::theme(
+             plot.background = ggplot2::element_blank(),
+             panel.grid.major = ggplot2::element_blank(),
+             panel.grid.minor = ggplot2::element_blank(),
+             panel.border = ggplot2::element_blank(),
+             panel.background = ggplot2::element_blank(),
+             axis.title.x = ggplot2::element_blank(),
+             axis.title.y = ggplot2::element_blank(),
+             axis.text.x = ggplot2::element_blank(),
+             axis.text.y = ggplot2::element_blank(),
+             axis.ticks = ggplot2::element_blank(),
+             axis.line = ggplot2::element_blank()
+           )
+         
            x<-dim[1]
            y<-dim[2]
            sets<-object@sets
@@ -67,20 +68,20 @@ setMethod("plot",signature="mfa",
 
            # plot compromise factor score of the two dimension ##############
            p1<-ggplot2::ggplot()+
-              ggplot2::geom_point(data=compromise,aes(x=compromise[,1],y=compromise[,2],color=id),size=3)+
-              ggplot2::theme(plot.title = element_text(size=10, face="bold",vjust=1,color="grey40"),
-                              axis.title.x = element_text(size=8, face="bold",vjust=-0.5,color="grey40"),
-                              axis.title.y = element_text(size=8, face="bold",vjust=0.5,color="grey40"),
-                              legend.title=element_blank(),
-                              panel.background = element_blank(),
-                             panel.grid.minor = element_line(colour = "grey90",size=0.2),
-                             panel.grid.major = element_line(colour = "grey90",size=0.2),
-                             legend.text=element_text(size=8))+
+              ggplot2::geom_point(data=compromise,ggplot2::aes(x=compromise[,1],y=compromise[,2],color=id),size=3)+
+              ggplot2::theme(plot.title = ggplot2::element_text(size=10, face="bold",vjust=1,color="grey40"),
+                              axis.title.x = ggplot2::element_text(size=8, face="bold",vjust=-0.5,color="grey40"),
+                              axis.title.y = ggplot2::element_text(size=8, face="bold",vjust=0.5,color="grey40"),
+                              legend.title=ggplot2::element_blank(),
+                              panel.background = ggplot2::element_blank(),
+                             panel.grid.minor = ggplot2::element_line(colour = "grey90",size=0.2),
+                             panel.grid.major = ggplot2::element_line(colour = "grey90",size=0.2),
+                             legend.text=ggplot2::element_text(size=8))+
               ggthemes::scale_color_calc()+
-              ggplot2::guides(color = guide_legend(keywidth = 0.9, keyheight = 0.9))+
+              ggplot2::guides(color = ggplot2::guide_legend(keywidth = 0.9, keyheight = 0.9))+
               # plot x and y axis
-              ggplot2::annotate("segment", x=-Inf,xend=Inf,y=0,yend=0,arrow=arrow(length=unit(0.3,"cm")),size=0.5,color="grey60") +
-              ggplot2::annotate("segment", y=-Inf,yend=Inf,x=0,xend=0,arrow=arrow(length=unit(0.3,"cm")),size=0.5,color="grey60")+
+              ggplot2::annotate("segment", x=-Inf,xend=Inf,y=0,yend=0,arrow=ggplot2::arrow(length=ggplot2::unit(0.3,"cm")),size=0.5,color="grey60") +
+              ggplot2::annotate("segment", y=-Inf,yend=Inf,x=0,xend=0,arrow=ggplot2::arrow(length=ggplot2::unit(0.3,"cm")),size=0.5,color="grey60")+
               # center (0,0)
               ggplot2::xlim(-max(abs(compromise[,1]))*1.1, max(abs(compromise[,1]))*1.1)+
               ggplot2::ylim(-max(abs(compromise[,2]))*1.1, max(abs(compromise[,2]))*1.1)+
@@ -89,42 +90,26 @@ setMethod("plot",signature="mfa",
 
 
            # plot barchart for eigenvalues
-          p2<-ggplot2::ggplot(data=eigen,aes(x=factor(id,levels=id),y=eigen,fill=factor(id,levels=id)))+
+          p2<-ggplot2::ggplot(data=eigen,ggplot2::aes(x=factor(id,levels=id),y=eigen,fill=factor(id,levels=id)))+
               ggplot2::geom_bar(stat="identity",width=0.5)+
               ggthemes::scale_fill_calc()+
-              ggplot2::theme(plot.title = element_text(size=10, face="bold",vjust=1,color="grey40"),
-                           axis.title.x = element_text(size=6, face="bold",vjust=-0.5,color="grey40"),
-                           axis.title.y = element_text(size=8, face="bold",vjust=0.5,color="grey40"),
-                           legend.title=element_blank(),
-                           panel.background = element_blank(),
-                           panel.grid.minor = element_line(colour = "grey90",size=0.2),
-                           panel.grid.major = element_line(colour = "grey90",size=0.2),
-                           axis.text.x = element_text(color="grey40",size=6),
-                           axis.text.y = element_text(color="grey40",size=8),
-                           legend.text=element_text(size=8))+
+              ggplot2::theme(plot.title = ggplot2::element_text(size=10, face="bold",vjust=1,color="grey40"),
+                           axis.title.x = ggplot2::element_text(size=6, face="bold",vjust=-0.5,color="grey40"),
+                           axis.title.y = ggplot2::element_text(size=8, face="bold",vjust=0.5,color="grey40"),
+                           legend.title=ggplot2::element_blank(),
+                           panel.background = ggplot2::element_blank(),
+                           panel.grid.minor = ggplot2::element_line(colour = "grey90",size=0.2),
+                           panel.grid.major = ggplot2::element_line(colour = "grey90",size=0.2),
+                           axis.text.x = ggplot2::element_text(color="grey40",size=6),
+                           axis.text.y = ggplot2::element_text(color="grey40",size=8),
+                           legend.text=ggplot2::element_text(size=8))+
               ggplot2::ggtitle(paste0("Eigenvalues"))+ggplot2::labs(x="",y="")+
-              ggplot2::guides(fill = guide_legend(keywidth = 0.9, keyheight = 0.9))
+              ggplot2::guides(fill = ggplot2::guide_legend(keywidth = 0.9, keyheight = 0.9))
 
 
 
 
 
-          # plot loadings of the two dimension ############
-          l<- ggplot2::ggplot()+
-              ggplot2::geom_point(data=loadings,aes(x=loadings[,1],y=loadings[,2],shape=group,color=group),size=2.5,stroke=1)+
-              ggplot2::scale_shape_manual(values=seq(0,9))+
-              ggplot2::theme(plot.title = element_text(size=13, face="bold",margin = margin(30, 30, 10, 0),color="grey40"),
-                             axis.title.x = element_text(size=10, face="bold",vjust=-0.35,color="grey40"),
-                             axis.title.y = element_text(size=10, face="bold",vjust=0.35,color="grey40"),
-                             legend.title=element_blank(),
-                             panel.grid.minor = element_line(colour = "white", size = 0.5))+
-              # plot x and y axis
-              ggplot2::annotate("segment", x=-Inf,xend=Inf,y=0,yend=0,arrow=arrow(length=unit(0.3,"cm")),size=0.5,color="grey60") +
-              ggplot2::annotate("segment", y=-Inf,yend=Inf,x=0,xend=0,arrow=arrow(length=unit(0.3,"cm")),size=0.5,color="grey60")+
-              # center (0,0)
-              ggplot2::xlim(-max(loadings[,1])*1.1, max(loadings[,1])*1.1)+
-              ggplot2::ylim(-max(loadings[,2])*1.1, max(loadings[,2])*1.1)+
-              ggplot2::ggtitle(paste0("Loadings"))+ggplot2::labs(x=names[1],y=names[2])
 
           # plot partial factor score
             plist <- list()
@@ -139,13 +124,22 @@ setMethod("plot",signature="mfa",
          # arrange output
             if (length(sets)==10){
               p3<-do.call(get("grid.arrange", asNamespace("gridExtra")),c(plist,ncol=5,top=""))
+              p<-gridExtra::grid.arrange(blankPlot,p1,p2,p3,ncol=6, nrow=2, widths=c(1,1,1,1,1,1), heights=c(3, 6),layout_matrix = rbind(c(1,3,3,2,2,1), c(4,4,4,4,4,4)))
+              
               jpeg("mfa.jpeg", width = 14, height = 9, units = 'in', res = 1000)
-              gridExtra::grid.arrange(blankPlot,p1,p2,p3,ncol=6, nrow=2, widths=c(1,1,1,1,1,1), heights=c(3, 6),layout_matrix = rbind(c(1,3,3,2,2,1), c(4,4,4,4,4,4)))
+              p
               dev.off()
+              print(p)
+              print(" 'mfa.jpeg' is saved in the current folder")
             }else{p3<-do.call(get("grid.arrange", asNamespace("gridExtra")),c(plist,ncol=floor(sqrt(length(sets))),top=""))
             jpeg("mfa.jpeg", width = 14, height = 9, units = 'in', res = 1000)
-            gridExtra::grid.arrange(p1,p2,p3,ncol=3)
+            p<-gridExtra::grid.arrange(p1,p2,p3,ncol=3)
+            
+            jpeg("mfa.jpeg", width = 14, height = 9, units = 'in', res = 1000)
+            p
             dev.off()
+            print(p)
+            print(" 'mfa.jpeg' is saved in the current folder")
             }
 
        })
