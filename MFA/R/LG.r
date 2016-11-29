ea3506362e7b600efca4b762f2ec8f996619d168
yizhouzhao <yizhou_zhao@berkeley.edu>
2016-11-29 15:57:10 -0800

    Update LG.r

--- MFA/R/LG.r
+++ MFA/R/LG.r
@@ -13,9 +13,16 @@
 
 # set funtion LG() to take two tables and return lg coefficient
 LG<-function(table1,table2){
+	#To check input data
+ 	table1<-as.matrix(table1)
+ 	if(!is.numeric(table1)) {stop("data should be numeric matrix or data.frame")}
+ 	table2<-as.matrix(table2)
+ 	if(!is.numeric(table2)) {stop("data should be numeric matrix or data.frame")}
+	
 	t1 <- t(table1)
 	t2 <- t(table2)
 	gamma1<-svd(table1)$d[1]
 	gamma2<-svd(table2)$d[1]
   sum(diag((table1 %*% t1) %*% (table2 %*% t2)))/(gamma1^2*gamma2^2)
+	
 }
