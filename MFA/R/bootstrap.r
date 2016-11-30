74dd5adb8513f58251a756118a78b46e7d0d69d0
Mingjia-Chen <mingjia.chen@yahoo.com>
2016-11-29 21:36:51 -0800

    debug...

--- MFA/R/bootstrap.r
+++ MFA/R/bootstrap.r
@@ -30,7 +30,7 @@ setMethod("bootstrap",signature="mfa",
           function(object,nbt=1000){
           if(nbt<=0||nbt%%1!=0) {stop("nbt should be a positive integer.")}
           groups<-length(object@partial_factor_score)
-          series<-sample(c(seq(1:10)),nbt*groups,TRUE)
+          series<-sample(c(seq(1:groups)),nbt*groups,TRUE)
           bts<-data.frame(table(series))
           F<-matrix(0,dim(object@partial_factor_score[[1]])[1],dim(object@partial_factor_score[[1]])[2])
           for (i in 1:groups){
@@ -41,7 +41,7 @@ setMethod("bootstrap",signature="mfa",
           for (i in 1:nbt){
             temp<-matrix(0,dim(object@partial_factor_score[[1]])[1],dim(object@partial_factor_score[[1]])[2])
             for (ii in 1:groups){
-              s<-(i-1)*10+ii
+              s<-(i-1)*groups+ii
               temp<-temp+object@partial_factor_score[[series[s]]]
             }
           F1<-F1+(temp/groups-MEAN)*(temp/groups-MEAN)
