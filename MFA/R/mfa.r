5165f632e6fe3396ade433c6e9bcb02b7309ebeb
ruonan-hao <ruonanhaornh@gmail.com>
2016-11-30 00:16:01 -0800

    new package

--- MFA/R/mfa.r
+++ MFA/R/mfa.r
@@ -6,14 +6,14 @@
 #' @rdname mfa-class
 #' @exportClass mfa
 setClass(
-Class="mfa",
-slots=list(
-sets="list",
-weights="numeric",
-eigenvalues="numeric",
-common_factor_score="matrix",
-partial_factor_score="list",
-loadings="matrix")
+  Class="mfa",
+  slots=list(
+    sets="list",
+    weights="numeric",
+    eigenvalues="numeric",
+    common_factor_score="matrix",
+    partial_factor_score="list",
+    loadings="matrix")
 )
 
 ############ constructor function: building the model #################
@@ -120,7 +120,7 @@ mfa<-function(data,sets,ncomps=NULL,center=TRUE,scale=TRUE){
     }
   }
   
-   
+  
   datarownames<-row.names(data)
   
   # scale and center
@@ -132,156 +132,117 @@ mfa<-function(data,sets,ncomps=NULL,center=TRUE,scale=TRUE){
     if(ncomps<=0||ncomps%%1!=0) {
       stop("ncomps should be positive integer.")
     }
-    if(!is.logical(scale)&&any(scale==0)) {stop("scale vector can't contain zero values.")}
-  }
-    #check sets
-  if(is.numeric(sets[[1]])){
-        check_sets<-NULL
-        for (i in 1:length(sets)) {
-            if(!is.numeric(sets[[i]])) {stop("sets should be a list of numeric vectors or character vectors.")}
-            check_sets<-c(check_sets,sets[[i]])
-        }
-        if(length(check_sets)!=ncol(data)) {stop("The sum of sets lengths does not equal to the number of columns.")}
-        if(any(!check_sets%in%1:ncol(data))) {stop("sets out of bounds")}
-        if(!identical(1:ncol(data),check_sets)) {warning("sets contain some overlapped and skipped columns.")}
+    if ((ncomps)>rank){
+      warning(paste0("ncomps larger than rank: outputing ",rank," dimensions."))
     }else{
-      if(is.character(sets[[1]])){
-            check_sets<-NULL
-            check_names<-NULL
-            for (i in 1:length(sets)){
-                if(!is.character(sets[[i]])) {stop("sets should be a list of character vectors or numeric vectors.")}
-                if(any(!sets[[i]]%in%colnames(data))) {stop("sets contain wrong variable names.")}
-                check_names<-c(check_names,sets[[i]])
-                check_sets<-c(check_sets,c(which(colnames(data)==sets[[i]][1]):which(colnames(data)==sets[[i]][length(sets[[i]])])))
-            }
-            if(length(check_sets)!=ncol(data)) {stop("The sum of sets lengths does not equal to the number of columns, or the variable names are in the wrong order.")}
-            if(!identical(colnames(data),check_names)) {warning("sets contain some overlapped and skipped columns.")}
-        }else{
-          stop("sets should be a list of numeric vectors or character vectors.")
-      }
+      rank<-ncomps
+    }
+  }else{
+    if (!rank%in%c(dim(data))){
+      message(paste0("Matrix is singular: outputing ",rank," dimensions."))
+    }
   }
-    
-    
-    datarownames<-row.names(data)
-    
-    # scale and center
-    data<-scale(data,center,scale)
-    
-    # check singularity and ncomps
-    rank<-Matrix::rankMatrix(data)[1]
-    if (!is.null(ncomps)){
-        if(ncomps<=0||ncomps%%1!=0) {
-            stop("ncomps should be positive integer.")
-        }
-        if ((ncomps)>rank){
-            warning(paste0("ncomps larger than rank: outputing ",rank," dimensions."))
-        }else{
-            rank<-ncomps
-        }
-    }else{
-        if (!rank%in%c(dim(data))){
-            message(paste0("Matrix is singular: outputing ",rank," dimensions."))
-        }
-    }
-    
-    # if sets is character: turn sets into indicies acccording to rownames of data
-    osets<-sets
-    if (!is.numeric(sets[[1]])){
-        newlist<-list()
-        for (i in 1:length(sets)){
-            newlist[[i]]<-c(which(colnames(data)==sets[[i]][1]):which(colnames(data)==sets[[i]][length(sets[[i]])]))
-        }
-        sets<-newlist
-    }
-    
-    # divide data into several group according to values in sets
-    # store the ith group of data to variable "Groupi"
-    for (i in 1:length(sets)) {
-        assign(paste0("Group",i),data.matrix(data[,min(sets[[i]]):max(sets[[i]])]))
-    }
-    
-    
-    # for each data groups conduct svd
-    # store the first singular values in singularvalues
-    singularvalues<-c(rep(1,dim(data)[1]))
-    for (i in 1:length(sets)) {
-        singularvalues[i]<-max(svd(eval(parse(text=paste0("Group",i))))$d)
-    }
-    
-    
-    # construct A to compute Q: QAQt=I
-    # A's diagonal elements are the inverse of the first square singular values
-    # each first square singular value is expanded to the same dimension of each data group
-    expanded<-c()
+  
+  # if sets is character: turn sets into indicies acccording to rownames of data
+  osets<-sets
+  if (!is.numeric(sets[[1]])){
+    newlist<-list()
     for (i in 1:length(sets)){
-        expanded<-c(expanded,rep(singularvalues[i],max(sets[[i]])-min(sets[[i]])+1))
+      newlist[[i]]<-c(which(colnames(data)==sets[[i]][1]):which(colnames(data)==sets[[i]][length(sets[[i]])]))
     }
-    weights<-1/expanded^2
-    A<-diag(x = 1/expanded^2,length(expanded),length(expanded))
-    A_half<-diag(x = 1/expanded,length(expanded),length(expanded))
-    A_half_inv<-diag( x = expanded,length(expanded),length(expanded))
-    
-    
-    
-    # construct M to compute P: PMPt=I
-    M<-diag(x=1/(dim(data)[1]),dim(data)[1],dim(data)[1])
-    M_half<-diag(x=1/sqrt((dim(data)[1])),dim(data)[1],dim(data)[1])
-    M_half_inv<-diag(x=sqrt((dim(data)[1])),dim(data)[1],dim(data)[1])
-    
-    
-    #  X: the whole data
-    X<-data.matrix(data)
-    # Construct S=XAXt
-    S<-as(X %*% A %*% t(X),"matrix")
-    
-    # do spectral decomposition on S: S=P*LAMBDA*Pt, PtMP=I
-    # construct inverse delta: delta^2=LAMBDA
-    eigens<-eigen(S)
-    d<-matrix(0,dim(X)[1],dim(X)[1])
-    for (i in 1:length(eigens$values)){
-        d[i,i]<-eigens$values[i]
-    }
-    u<-eigens$vectors
-    lambda<-as(M_half %*% d %*% M_half,"matrix")
-    
-    delta_value<-diag(as(sqrt(lambda),"matrix"))
-    
-    delta_inv<-1/sqrt(lambda)
-    delta_inv[is.infinite(delta_inv)]<-0
-    
-    # P is PMPt=I FOR S=P*LAMBDA*Pt
-    P <- as(u %*% M_half_inv,"matrix")
-    # Q FOR Q=Xt*M*P*DELTA_inverse
-    Q <- as(t(X) %*% M %*% P %*% delta_inv, "matrix")[,1:rank]
-    dimnames(Q) <- list(rownames(Q),colnames(Q, do.NULL = FALSE, prefix = "Dim"))
-    
-    
-    # build a list: 'partial_factor_score' to store partial factor score
-    # build a matrix: 'common_factor_score' to store common factor score
-    # the partial score of group i is named "Partial Score: Group i"
-    # partial factor score i = no. of group * A_i* data group i* Q_i
-    # common factor score = sum of partial factor score i
-    
-    
-    partial_factor_score<-list()
-    common_factor_score<-0
-    for (i in 1:length(sets)){
-        datai<- data.matrix(eval(parse(text=paste0("Group",i))))
-        score<-length(sets) * (1/singularvalues[i]^2) * datai %*% t(datai)  %*% M %*% P %*% delta_inv[,1:rank]
-        dimnames(score) <- list(datarownames,colnames(score, do.NULL = FALSE, prefix = "Dim"))
-        partial_factor_score[[paste0("Partial Score: Group ",i)]]=as(score,"matrix")
-        common_factor_score<-score+common_factor_score
-    }
-    common_factor_score<-common_factor_score/length(sets)
-    
-    
-    # loading uses Q
-    new (Class = "mfa",
-    sets=osets,
-    eigenvalues = c(delta_value^2)[1:rank],
-    weights= weights,
-    common_factor_score = as(common_factor_score,"matrix"),
-    partial_factor_score = partial_factor_score,
-    loadings = as(Q,"matrix")
-    )
+    sets<-newlist
+  }
+  
+  # divide data into several group according to values in sets
+  # store the ith group of data to variable "Groupi"
+  for (i in 1:length(sets)) {
+    assign(paste0("Group",i),data.matrix(data[,min(sets[[i]]):max(sets[[i]])]))
+  }
+  
+  
+  # for each data groups conduct svd
+  # store the first singular values in singularvalues
+  singularvalues<-c(rep(1,dim(data)[1]))
+  for (i in 1:length(sets)) {
+    singularvalues[i]<-max(svd(eval(parse(text=paste0("Group",i))))$d)
+  }
+  
+  
+  # construct A to compute Q: QAQt=I
+  # A's diagonal elements are the inverse of the first square singular values
+  # each first square singular value is expanded to the same dimension of each data group
+  expanded<-c()
+  for (i in 1:length(sets)){
+    expanded<-c(expanded,rep(singularvalues[i],max(sets[[i]])-min(sets[[i]])+1))
+  }
+  weight<-1/expanded^2
+  A<-diag(x = 1/expanded^2,length(expanded),length(expanded))
+  A_half<-diag(x = 1/expanded,length(expanded),length(expanded))
+  A_half_inv<-diag( x = expanded,length(expanded),length(expanded))
+  
+  
+  
+  # construct M to compute P: PMPt=I
+  M<-diag(x=1/(dim(data)[1]),dim(data)[1],dim(data)[1])
+  M_half<-diag(x=1/sqrt((dim(data)[1])),dim(data)[1],dim(data)[1])
+  M_half_inv<-diag(x=sqrt((dim(data)[1])),dim(data)[1],dim(data)[1])
+  
+  
+  #  X: the whole data
+  X<-data.matrix(data)
+  # Construct S=XAXt
+  S<-as(X %*% A %*% t(X),"matrix")
+  
+  # do spectral decomposition on S: S=P*LAMBDA*Pt, PtMP=I
+  # construct inverse delta: delta^2=LAMBDA
+  eigens<-eigen(S)
+  d<-matrix(0,dim(X)[1],dim(X)[1])
+  for (i in 1:length(eigens$values)){
+    d[i,i]<-eigens$values[i]
+  }
+  u<-eigens$vectors
+  lambda<-as(M_half %*% d %*% M_half,"matrix")
+  
+  lambda[,(rank+1):ncol(lambda)] <- 0
+  delta_value<-diag(as(sqrt(lambda),"matrix"))
+  
+  delta_inv<-1/sqrt(lambda)
+  delta_inv[is.infinite(delta_inv)]<-0
+  
+  # P is PMPt=I FOR S=P*LAMBDA*Pt
+  P <- as(u %*% M_half_inv,"matrix")
+  # Q FOR Q=Xt*M*P*DELTA_inverse
+  Q <- as(t(X) %*% M %*% P %*% delta_inv, "matrix")[,1:rank]
+  dimnames(Q) <- list(rownames(Q),colnames(Q, do.NULL = FALSE, prefix = "Dim"))
+  
+  
+  # build a list: 'partial_factor_score' to store partial factor score
+  # build a matrix: 'common_factor_score' to store common factor score
+  # the partial score of group i is named "Partial Score: Group i"
+  # partial factor score i = no. of group * A_i* data group i* Q_i
+  # common factor score = sum of partial factor score i
+  
+  
+  partial_factor_score<-list()
+  common_factor_score<-0
+  for (i in 1:length(sets)){
+    datai<- data.matrix(eval(parse(text=paste0("Group",i))))
+    score<-length(sets) * (1/singularvalues[i]^2) * datai %*% t(datai)  %*% M %*% P %*% delta_inv[,1:rank]
+    dimnames(score) <- list(datarownames,colnames(score, do.NULL = FALSE, prefix = "Dim"))
+    partial_factor_score[[paste0("Partial Score: Group ",i)]]=as(score,"matrix")
+    common_factor_score<-score+common_factor_score
+  }
+  common_factor_score<-common_factor_score/length(sets)
+  
+  
+  # loading uses Q
+  new (Class = "mfa",
+       sets=osets,
+       eigenvalues = c(delta_value^2)[1:rank],
+       weights= weight,
+       common_factor_score = as(common_factor_score,"matrix"),
+       partial_factor_score = partial_factor_score,
+       loadings = as(Q,"matrix")
+  )
 }
+
