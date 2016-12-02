49ca690e78e162fb2a89d76733990ca24e3c5f83
Lee <Lee@LeedeAir-2.attlocal.net>
2016-12-01 23:55:52 -0800

    update shiny

--- MFA/ShinyApp/plotfunctions.R
+++ MFA/ShinyApp/plotfunctions.R
@@ -16,18 +16,18 @@ plot_eigen <- function(x){
   p2<-ggplot2::ggplot(data=eigen,ggplot2::aes(x=factor(id,levels=id),y=eigen,fill=factor(id,levels=id)))+
     ggplot2::geom_bar(stat="identity",width=0.5)+
     ggthemes::scale_fill_calc()+
-    ggplot2::theme(plot.title = ggplot2::element_text(size=18, face="bold",vjust=1,color="grey40"),
-                   axis.title.x = ggplot2::element_text(size=12, face="bold",vjust=-0.5,color="grey40"),
-                   axis.title.y = ggplot2::element_text(size=12, face="bold",vjust=0.5,color="grey40"),
+    ggplot2::theme(plot.title = ggplot2::element_text(size=22, face="bold",vjust=1,color="grey40"),
+                   axis.title.x = ggplot2::element_text(size=18, face="bold",vjust=-0.5,color="grey40"),
+                   axis.title.y = ggplot2::element_text(size=18, face="bold",vjust=0.5,color="grey40"),
                    legend.title=ggplot2::element_blank(),
                    panel.background = ggplot2::element_blank(),
-                   panel.grid.minor = ggplot2::element_line(colour = "grey90",size=0.2),
-                   panel.grid.major = ggplot2::element_line(colour = "grey90",size=0.2),
-                   axis.text.x = ggplot2::element_text(color="grey40",size=6),
-                   axis.text.y = ggplot2::element_text(color="grey40",size=8),
-                   legend.text=ggplot2::element_text(size=8))+
+                   panel.grid.minor = ggplot2::element_line(colour = "grey90",size=0.4),
+                   panel.grid.major = ggplot2::element_line(colour = "grey90",size=0.4),
+                   axis.text.x = ggplot2::element_text(color="grey40",size=12),
+                   axis.text.y = ggplot2::element_text(color="grey40",size=12),
+                   legend.text=ggplot2::element_text(size=12))+
     ggplot2::ggtitle(paste0("Eigenvalues"))+ggplot2::labs(x="",y="")+
-    ggplot2::guides(fill = ggplot2::guide_legend(keywidth = 0.9, keyheight = 0.9))
+    ggplot2::guides(fill = ggplot2::guide_legend(keywidth = 1.8, keyheight = 1.8))
     p2
   
 }
@@ -40,25 +40,25 @@ plot_common <- function(x,dim){
   compromise$id<-rownames(compromise)
   # plot compromise factor score of the two dimension ##############
   p1<-ggplot2::ggplot()+
-    ggplot2::geom_point(data=compromise,ggplot2::aes(x=compromise[,1],y=compromise[,2],color=id),size=3)+
-    ggplot2::theme(plot.title = ggplot2::element_text(size=18, face="bold",vjust=1,color="grey40"),
-                   axis.title.x = ggplot2::element_text(size=12, face="bold",vjust=-0.5,color="grey40"),
-                   axis.title.y = ggplot2::element_text(size=12, face="bold",vjust=0.5,color="grey40"),
+    ggplot2::geom_point(data=compromise,ggplot2::aes(x=compromise[,1],y=compromise[,2],color=id),size=6)+
+    ggplot2::theme(plot.title = ggplot2::element_text(size=22, face="bold",vjust=1,color="grey50"),
+                   axis.title.x = ggplot2::element_text(size=18, face="bold",vjust=-0.5,color="grey50"),
+                   axis.title.y = ggplot2::element_text(size=18, face="bold",vjust=0.5,color="grey50"),
                    legend.title=ggplot2::element_blank(),
                    panel.background = ggplot2::element_blank(),
-                   panel.grid.minor = ggplot2::element_line(colour = "grey90",size=0.2),
-                   panel.grid.major = ggplot2::element_line(colour = "grey90",size=0.2),
-                   legend.text=ggplot2::element_text(size=8))+
+                   panel.grid.minor = ggplot2::element_line(colour = "grey90",size=0.4),
+                   panel.grid.major = ggplot2::element_line(colour = "grey90",size=0.4),
+                   legend.text=ggplot2::element_text(size=12))+
     ggthemes::scale_color_calc()+
-    ggplot2::guides(color = ggplot2::guide_legend(keywidth = 0.9, keyheight = 0.9))+
+    ggplot2::guides(color = ggplot2::guide_legend(keywidth = 1.8, keyheight = 1.8))+
     # plot x and y axis
-    ggplot2::annotate("segment", x=-Inf,xend=Inf,y=0,yend=0,arrow=ggplot2::arrow(length=ggplot2::unit(0.3,"cm")),size=0.5,color="grey60") +
-    ggplot2::annotate("segment", y=-Inf,yend=Inf,x=0,xend=0,arrow=ggplot2::arrow(length=ggplot2::unit(0.3,"cm")),size=0.5,color="grey60")+
+    ggplot2::annotate("segment", x=-Inf,xend=Inf,y=0,yend=0,arrow=ggplot2::arrow(length=ggplot2::unit(0.3,"cm")),size=0.8,color="grey60") +
+    ggplot2::annotate("segment", y=-Inf,yend=Inf,x=0,xend=0,arrow=ggplot2::arrow(length=ggplot2::unit(0.3,"cm")),size=0.8,color="grey60")+
     # center (0,0)
     ggplot2::xlim(-max(abs(compromise[,1]))*1.1, max(abs(compromise[,1]))*1.1)+
     ggplot2::ylim(-max(abs(compromise[,2]))*1.1, max(abs(compromise[,2]))*1.1)+
     ggplot2::ggtitle(paste0("Compromise Factor Score"))+ggplot2::labs(x=names[1],y=names[2])
-    p1
+  p1
 }
 
 #3.partial factor scores
@@ -67,22 +67,24 @@ partial_plot<-function(group_num,data,data2,names){
   p = ggplot2::ggplot()+
     ## ?refer color table
     ggplot2::geom_point(data=data,ggplot2::aes(x=data[,1],y=data[,2]),color = defaultColors[1:length(data[,1])],size=2)+
+    ggplot2::scale_color_manual(name = element_blank(),labels=rownames(data),values = defaultColors[1:length(data[,1])])+
     ggplot2::geom_point(data=data2,ggplot2::aes(x=data2[,1],y=data2[,2]),color="grey10",shape=17,size=1.5)+
     ggplot2::geom_text(data=data2,ggplot2::aes(x=data2[,1],y=data2[,2],label=rownames(data2)),size=2,color="black",hjust=-0.15, vjust=-0.05)+
     ggplot2::scale_shape_manual(values=group_num+1)+
-    ggplot2::theme(plot.title = ggplot2::element_text(size=12, face="bold",vjust=0.05,color="grey40"),
-                   axis.title.x = ggplot2::element_text(size=12, face="bold",vjust=-0.05,color="grey40"),
-                   axis.title.y = ggplot2::element_text(size=12, face="bold",vjust=0.05,color="grey40"),
-                   legend.position="none",
-                   axis.text.x = ggplot2::element_text(color="grey40",size=8),
-                   axis.text.y = ggplot2::element_text(color="grey40",size=8),
+    ggplot2::theme(plot.title = ggplot2::element_text(size=22, face="bold",vjust=0.05,color="grey40"),
+                   axis.title.x = ggplot2::element_text(size=18, face="bold",vjust=-0.05,color="grey40"),
+                   axis.title.y = ggplot2::element_text(size=18, face="bold",vjust=0.05,color="grey40"),
+                   axis.text.x = ggplot2::element_text(color="grey40",size=12),
+                   axis.text.y = ggplot2::element_text(color="grey40",size=12),
+                   legend.title=ggplot2::element_blank(),
+                   legend.text=ggplot2::element_text(size=12),
                    panel.grid.minor = ggplot2::element_blank(),
-                   panel.grid.major = ggplot2::element_line(colour = "grey90",size=0.1),
+                   panel.grid.major = ggplot2::element_line(colour = "grey90",size=0.4),
                    panel.background = ggplot2::element_blank())+
     # plot x and y axis
     ggthemes::scale_color_calc()+
-    ggplot2::annotate("segment", x=-Inf,xend=Inf,y=0,yend=0,arrow=ggplot2::arrow(length=ggplot2::unit(0.2,"cm")),size=0.3,color="grey50") +
-    ggplot2::annotate("segment", y=-Inf,yend=Inf,x=0,xend=0,arrow=ggplot2::arrow(length=ggplot2::unit(0.2,"cm")),size=0.3,color="grey50")+
+    ggplot2::annotate("segment", x=-Inf,xend=Inf,y=0,yend=0,arrow=ggplot2::arrow(length=ggplot2::unit(0.3,"cm")),size=0.5,color="grey50") +
+    ggplot2::annotate("segment", y=-Inf,yend=Inf,x=0,xend=0,arrow=ggplot2::arrow(length=ggplot2::unit(0.3,"cm")),size=0.5,color="grey50")+
     # center (0,0)
     ggplot2::xlim(-max(abs(c(data[,1],data2[,1])))*1.2, max(abs(c(data[,1],data2[,1])))*1.2)+
     ggplot2::ylim(-max(abs(c(data[,2],data2[,2])))*1.2, max(abs(c(data[,2],data2[,2])))*1.2)+
